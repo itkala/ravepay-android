344917fa1f2fecaac476ccdbe7b893b123812fcb
Oluwatayo <oluwatayoadegboye@gmail.com>
2019-01-10 09:26:23 +0100

    add rave project

--- app/src/main/java/ng/com/teddinsight/libtest/MainActivity.java
+++ app/src/main/java/ng/com/teddinsight/libtest/MainActivity.java
@@ -1,7 +1,10 @@
 package ng.com.teddinsight.libtest;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.flutterwave.raveandroid.RavePayManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
